ab7715a2bfe857d9ae8405630939de79289b9a49
YuliyaValova <Lulkaval@gmail.com>
2021-11-28 15:21:33 +0300

    EncodingFilter added

---
+++ controller/src/main/java/com/jwd/controller/filters/EncodingFilter.java
@@ -0,0 +1,33 @@
+package com.jwd.controller.filters;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class EncodingFilter implements Filter {
+
+    private static final String FILTERABLE_CONTENT_TYPE = "application/x-www-form-urlencoded";
+    private static final String ENCODING_DEFAULT = "UTF-8";
+    private static final String ENCODING_INIT_PARAM = "encoding";
+    private String encoding;
+
+    @Override
+    public void init(FilterConfig filterConfig){
+        encoding = filterConfig.getInitParameter(ENCODING_INIT_PARAM);
+        if (encoding == null) {
+            encoding = ENCODING_DEFAULT;
+        }
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        String contentType = servletRequest.getContentType();
+        if (contentType != null && contentType.startsWith(FILTERABLE_CONTENT_TYPE)) {
+            servletRequest.setCharacterEncoding(encoding);
+        }
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
