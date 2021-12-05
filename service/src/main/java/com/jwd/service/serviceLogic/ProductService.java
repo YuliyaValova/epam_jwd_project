8cbeeca543fe3779e26f2a72d34414128236cc30
YuliyaValova <Lulkaval@gmail.com>
2021-12-06 01:15:00 +0300

    Service javadoc

--- service/src/main/java/com/jwd/service/serviceLogic/ProductService.java
+++ service/src/main/java/com/jwd/service/serviceLogic/ProductService.java
@@ -8,33 +8,121 @@ import com.jwd.service.exception.ServiceException;
 
 public interface ProductService {
 
+    /**
+     * Makes a page for showing available products in a menu
+     * @param productPageRequest required pages params
+     * @return product page
+     * @throws ServiceException is a module exception
+     */
     Page<Product> showProducts(Page<Product> productPageRequest) throws ServiceException;
 
+    /**
+     * Makes a page for showing products in user's basket
+     * @param pageRequest required pages params
+     * @param id identifies user
+     * @return product page
+     * @throws ServiceException is a module exception
+     */
     Page<Product> showBasket(Page<Product> pageRequest, long id) throws ServiceException;
 
+    /**
+     * Deletes all products from user's basket
+     * @param id identifies user
+     * @throws ServiceException is a module exception
+     */
     void cleanBasket(long id) throws ServiceException;
 
+    /**
+     * Calculates the total cost of the user's basket
+     * @param id identifies user
+     * @return total sum
+     * @throws ServiceException is a module exception
+     */
     double getSum(long id) throws ServiceException;
 
+    /**
+     * Changes the status of all items in the user's basket to "Paid up"
+     * @param id identifies user
+     * @throws ServiceException is a module exception
+     */
     void sendOrder(long id) throws ServiceException;
 
+    /**
+     * Delete from user's basket selected item
+     * @param id identifies user
+     * @param productId identifies product for deleting
+     * @throws ServiceException is a module exception
+     */
     void deleteFromBasket(long id, long productId) throws ServiceException;
 
+    /**
+     * Adds a product to user's basket
+     * @param id identifies user
+     * @param productId identifies product for adding
+     * @return true if adding is successful, otherwise - false
+     * @throws ServiceException is a module exception
+     */
     boolean addToBasket(long id, long productId) throws ServiceException;
 
+    /**
+     * Add a product to a menu
+     * @param product for adding
+     * @return -2 if product is invalid, -1 if such product already exists, 1 if adding is successful
+     * @throws ServiceException is a module exception
+     */
     int addProductToMenu(Product product) throws ServiceException;
 
+    /**
+     * Converts String param to double
+     * @param parameter for converting
+     * @return double value
+     */
     double convertStringToDouble(String parameter);
 
+    /**
+     * Makes a page for showing paid orders to admin
+     * @param pageRequest required page params
+     * @return order page
+     * @throws ServiceException is a module exception
+     */
     PageOrder<Order> showPaidOrders(PageOrder<Order> pageRequest) throws ServiceException;
 
+    /**
+     * Makes a page for showing all orders to admin (if ordered product wasn't deleted)
+     * @param pageRequest required page params
+     * @return order page
+     * @throws ServiceException is a module exception
+     */
     PageOrder<Order> showAllOrders(PageOrder<Order> pageRequest) throws ServiceException;
 
+    /**
+     * Deletes a product from menu
+     * @param productId identifies a product for deleting
+     * @throws ServiceException is a module exception
+     */
     void deleteFromMenu(long productId) throws ServiceException;
 
+    /**
+     * Finds a product by id
+     * @param productId identifies a product for searching
+     * @return product
+     * @throws ServiceException is a module exception
+     */
     Product findProduct(long productId) throws ServiceException;
 
+    /**
+     * Changes orders status to the specified
+     * @param orderId identifies a order for updating
+     * @param status new status
+     * @return true if changing is successful, otherwise - false
+     * @throws ServiceException is a module exception
+     */
     boolean changeStatus(long orderId, String status) throws ServiceException;
 
+    /**
+     * Converts String param to boolean
+     * @param status value for converting
+     * @return boolean value
+     */
     boolean convertStringToBoolean(String status);
 }
--- service/src/main/java/com/jwd/service/serviceLogic/UserService.java
+++ service/src/main/java/com/jwd/service/serviceLogic/UserService.java
@@ -6,15 +6,52 @@ import com.jwd.service.exception.ServiceException;
 
 public interface UserService {
 
+    /**
+     * Registers a new user
+     * @param user to be registered
+     * @return -2 if user is not valid, -1 if such user already exists, 1 if registration is successful
+     * @throws ServiceException is a module exception
+     */
     int register(UserAccount user) throws ServiceException;
 
+    /**
+     * Lets the user into the system
+     * @param user to be sign in
+     * @return logged user account
+     * @throws ServiceException is a module exception
+     */
     UserAccount login(UserAccount user) throws ServiceException;
 
+    /**
+     * Deletes user account by id
+     * @param id for searching account in db
+     * @throws ServiceException is a module exception
+     */
     void deleteAccount(long id) throws ServiceException;
 
+    /**
+     * Changes user's password
+     * @param id for searching that user in db
+     * @param oldPassword that is changed
+     * @param newPassword that will be installed
+     * @return true if change is successful, otherwise - false
+     * @throws ServiceException is a module exception
+     */
     boolean changePassword(long id, String oldPassword, String newPassword) throws ServiceException;
 
+    /**
+     * Sets administrator rights to the specified user
+     * @param userId identifies the user to be searched for
+     * @return true if appointment was successful, otherwise - false
+     * @throws ServiceException is a module exception
+     */
     boolean addAdmin(long userId) throws ServiceException;
 
+    /**
+     * Finds user by id
+     * @param userId for searching user in db
+     * @return searching user account
+     * @throws ServiceException is a module exception
+     */
     UserAccount findUser(long userId) throws ServiceException;
 }
