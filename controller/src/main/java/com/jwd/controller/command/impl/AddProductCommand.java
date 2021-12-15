1d1e6adfe2df2815a81f72e9aa71b0c0cce35750
YuliyaValova <Lulkaval@gmail.com>
2021-12-15 15:50:01 +0300

    Navigation refactor

--- controller/src/main/java/com/jwd/controller/command/impl/AddProductCommand.java
+++ controller/src/main/java/com/jwd/controller/command/impl/AddProductCommand.java
@@ -46,7 +46,6 @@ public class AddProductCommand implements Command {
                         resp.sendRedirect("addProduct?message=IncompleteInfo");
                         break;
                     case 1:
-                        req.setAttribute(PAGE, SHOW);
                         req.setAttribute(MESSAGE, PRODUCT_SUCCESSFULLY_ADDED);
                         req.getRequestDispatcher(PATH_TO_JSP + Command.prepareUri(req) + JSP).forward(req, resp);
                         break;
